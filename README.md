556f74dd88b37eee94843f361e56da798c796cfb
David Keyes <david@rfortherestofus.com>
2023-05-16 15:45:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rmedicine-parameterized-reporting
+ 
---
+++ dk/temp.R
@@ -0,0 +1,23 @@
+# Load packages
+library(tidyverse)
+library(NHSRdatasets)
+library(quarto)
+
+# Create a vector of all locations
+locations <- ae_attendances %>%
+  distinct(org_code) %>% 
+  pull(org_code) 
+
+# Create a tibble with information on the:
+# input R Markdown document
+# output HTML file
+# parameters needed to knit the document
+reports <- tibble(
+  input = "report.qmd",
+  output_file = str_glue("{locations}.html"),
+  execute_params = map(locations, ~list(location = .))
+)
+
+# Generate all of our reports
+reports %>%
+  walk(quarto_render)
